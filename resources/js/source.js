8889dcfad2f3f2c57077965ac3ba31717fec4a8c
JM <jmperez@thethings.io>
2019-11-01 02:49:28 +0100

    improvements

--- resources/js/source.js
+++ resources/js/source.js
@@ -2,48 +2,26 @@
 
 window.onload = function() {
 
-    //Default Grid Settings
-    let settings = {
-        screenW: window.innerWidth - 100,
-        screenH: window.innerHeight - 100,
-        hexSize: 36,
-        hexOrientation: 'flat',
-        hexColums: Math.ceil((window.innerWidth - 100) / 54), // x
-        hexRows:  Math.ceil((window.innerHeight - 100) / (36*1.731)), // y
-        lineThickness: 2,
-        lineColor: 0x999999,
-        hideCoords: false
-    };
-
-    $('#hexSize').val(settings.hexSize);
-    $('#hexOrientation').val(settings.hexOrientation);
-    $('#hexColums').val(settings.hexColums);
-    $('#hexRows').val(settings.hexRows);
-    $('#lineThickness').val(settings.lineThickness);
+    let settings = getDefaultGridSettings();
+    updateSettingsModal(settings);
 
     let canvas = document.getElementById("canvas");
     let app = new PIXI.Application({ width: settings.screenW, height: settings.screenH, transparent: true, preserveDrawingBuffer:true, view: canvas });
 
-    // create viewport
-    const viewport = new Viewport.Viewport({
-        screenWidth: app.view.offsetWidth,
-        screenHeight: app.view.offsetHeight,
-        worldWidth: settings.hexColums * (settings.hexSize + (settings.hexSize / 2)) + (settings.hexSize / 2),
-        worldHeight: settings.hexRows * (settings.hexSize * 1.731) + (settings.hexSize * 1.731 / 2),
-
-        interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
-    });
-
-    app.stage.addChild(viewport);
-
-    // activate plugins
-    viewport
-        .drag()
-        .bounce();
+    const viewport = initializeViewport(app, settings);
 
     loadGrid(app, viewport, settings);
 
     $("#gridSettingsModal").submit(function(){
+        viewport.destroy({children: true});
+        for (let i = app.stage.children.length - 1; i >= 0; i--) {
+            app.stage.removeChild(app.stage.children[i]);
+        }
+        applySettings(app);
+        return false;
+    });
+
+    $("#noiseSettingsModal").submit(function(){
         viewport.destroy({children: true});
         for (let i = app.stage.children.length - 1; i >= 0; i--) {
             app.stage.removeChild(app.stage.children[i]);
@@ -60,53 +38,63 @@ window.onload = function() {
         applySettings(app);
         return false;
     });
+
+    window.onresize = function() {
+        let width = ( window.innerWidth - 100 > 1140 ) ? 1140 : window.innerWidth - 100;
+        let height = window.innerHeight - 100;
+        app.renderer.resize(width, height);
+    };
 };
 
 function loadGrid(app, viewport, settings) {
-    let graphics = new PIXI.Graphics();
     let Hex = Honeycomb.extendHex({ size: settings.hexSize,  orientation: settings.hexOrientation });
-    //let Hex = Honeycomb.extendHex({ size: {width: 72, height: 72},  orientation: settings.hexOrientation });
     let Grid = Honeycomb.defineGrid(Hex);
     let elevation = heightMap(settings);
-
-    // set a line style of 1px wide and color #999
-    //graphics.lineStyle(settings.lineThickness, settings.lineColor);
+    let gridColor = 0x000000;
 
     // render hex grid
     let gr = Grid.rectangle({ width: settings.hexColums, height: settings.hexRows });
     gr.forEach((hex, index) => {
-
+        let graphics = new PIXI.Graphics();
         let coords = hex.cartesian();
         if (elevation[coords.x][coords.y] < -1.0) {
-            graphics.lineStyle(settings.lineThickness, 0xB0E0E6);
+            if (settings.hideGrid) gridColor = 0xB0E0E6;
+            graphics.lineStyle(settings.lineThickness, gridColor);
             graphics.beginFill(0xB0E0E6);
         }
         else if (elevation[coords.x][coords.y] < -0.2) {
-            graphics.lineStyle(settings.lineThickness, 0x8FBC8F);
+            if (settings.hideGrid) gridColor = 0x8FBC8F;
+            graphics.lineStyle(settings.lineThickness, gridColor);
             graphics.beginFill(0x8FBC8F);
         }
         else if (elevation[coords.x][coords.y] < 0.2) {
-            graphics.lineStyle(settings.lineThickness, 0x2E8B57);
+            if (settings.hideGrid) gridColor = 0x2E8B57;
+            graphics.lineStyle(settings.lineThickness, gridColor);
             graphics.beginFill(0x2E8B57);
         }
         else if (elevation[coords.x][coords.y] < 0.4) {
-            graphics.lineStyle(settings.lineThickness, 0x895543);
+            if (settings.hideGrid) gridColor = 0x895543;
+            graphics.lineStyle(settings.lineThickness, gridColor);
             graphics.beginFill(0x895543);
         }
         else if (elevation[coords.x][coords.y] < 0.6) {
-            graphics.lineStyle(settings.lineThickness, 0x8A4533);
+            if (settings.hideGrid) gridColor = 0x8A4533;
+            graphics.lineStyle(settings.lineThickness, gridColor);
             graphics.beginFill(0x8A4533);
         }
         else if (elevation[coords.x][coords.y] < 0.75) {
-            graphics.lineStyle(settings.lineThickness, 0x8B3503);
+            if (settings.hideGrid) gridColor = 0x8B3503;
+            graphics.lineStyle(settings.lineThickness, gridColor);
             graphics.beginFill(0x8B3503);
         }
         else if (elevation[coords.x][coords.y] < 0.90) {
-            graphics.lineStyle(settings.lineThickness, 0x8B4513);
+            if (settings.hideGrid) gridColor = 0x8B4513;
+            graphics.lineStyle(settings.lineThickness, gridColor);
             graphics.beginFill(0x8B4513);
         }
         else {
-            graphics.lineStyle(settings.lineThickness, 0xDCDCDC);
+            if (settings.hideGrid) gridColor = 0xDCDCDC;
+            graphics.lineStyle(settings.lineThickness, gridColor);
             graphics.beginFill(0xDCDCDC);
         }
 
@@ -123,9 +111,7 @@ function loadGrid(app, viewport, settings) {
         // finish at the first corner
         graphics.lineTo(firstCorner.x, firstCorner.y);
 
-        if (index % 2 === 0) {
-          graphics.endFill();
-        }
+        graphics.endFill();
 
         viewport.addChild(graphics);
     });
@@ -148,49 +134,31 @@ function loadGrid(app, viewport, settings) {
 
         viewport.addChild(text);
     });
-
-
 }
 
 function applySettings(app, viewport) {
+
+    let width = ( window.innerWidth - 100 > 1140 ) ? 1140 : window.innerWidth - 100;
+    let height = window.innerHeight - 100;
+
     let settings = {};
-    settings.screenW = window.innerWidth - 100;
-    settings.screenH = window.innerHeight - 100;
+    settings.screenW = width;
+    settings.screenH = height - 100;
     settings.hexSize = parseInt($('#hexSize').val()) || 36;
     settings.hexOrientation = $('#hexOrientation').val() || 'flat';
-    settings.hexColums = parseInt($('#hexColums').val()) || (window.innerWidth - 100) / 54;
-    settings.hexRows = parseInt($('#hexRows').val()) || (window.innerHeight - 100) / 72;
+    settings.hexColums = parseInt($('#hexColums').val()) || (width - 100) / 54;
+    settings.hexRows = parseInt($('#hexRows').val()) || (height - 100) / 72;
     settings.lineThickness = parseInt($('#lineThickness').val()) || 2;
     settings.lineColor = 0x999999;
     settings.hideCoords = $('#hideCoords').is(":checked");
+    settings.hideGrid = $('#hideGrid').is(":checked");
 
-    let worldWidth = settings.hexColums * (settings.hexSize + (settings.hexSize / 2)) + (settings.hexSize / 2);
-    let worldHeight = settings.hexRows * (settings.hexSize * 1.731) + (settings.hexSize * 1.731 / 2);
-    if (settings.hexOrientation === 'pointy') {
-        worldWidth = settings.hexColums * (settings.hexSize * 1.731) + (settings.hexSize * 1.731 / 2);
-        worldHeight = settings.hexRows * (settings.hexSize + (settings.hexSize / 2)) + (settings.hexSize / 2);
-    }
-
-    // create viewport
-    viewport = new Viewport.Viewport({
-        screenWidth: app.view.offsetWidth,
-        screenHeight: app.view.offsetHeight,
-        worldWidth: worldWidth,
-        worldHeight: worldHeight,
-
-        interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
-    });
-
-    app.stage.addChild(viewport);
-
-    // activate plugins
-    viewport
-        .drag()
-        .bounce();
+    viewport = initializeViewport(app, settings);
 
     loadGrid(app, viewport, settings);
 
     $("#gridSettingsModal").modal("hide");
+    $("#noiseSettingsModal").modal("hide");
 }
 
 function downloadCanvasAsPng() {
@@ -198,7 +166,10 @@ function downloadCanvasAsPng() {
 }
 
 function heightMap(settings) {
-    const simplex = new SimplexNoise();
+    let seed = generateId();
+    if ($('#setSeed').is(":checked")) seed = $('#seed').val();
+    else $('#seed').val(seed);
+    const simplex = new SimplexNoise(seed);
     let elevation = [[]];
     let freq = 0.8;
     for (let x = 0; x < settings.hexColums; x++) {
@@ -213,4 +184,74 @@ function heightMap(settings) {
     }
 
     return elevation;
+}
+
+function getDefaultGridSettings() {
+
+    let width = ( window.innerWidth - 100 > 1140 ) ? 1140 : window.innerWidth - 100;
+    let height = window.innerHeight - 100;
+    let colums = ( Math.ceil( width / 15 ) > 75) ? 75 : Math.ceil( width / 15 );
+    let rows = ( Math.ceil( height / ( 10 * 1.731 ) ) > 30) ? 30 : Math.ceil( height / ( 10 * 1.731 ) );
+
+    return {
+        screenW: width,
+        screenH: height,
+        hexSize: 10,
+        hexOrientation: 'flat',
+        hexColums: colums, // x
+        hexRows:  rows, // y
+        lineThickness: 1,
+        lineColor: 0x999999,
+        hideCoords: true,
+        hideGrid: false
+    }
+}
+
+function updateSettingsModal(settings) {
+    $('#hexSize').val(settings.hexSize);
+    $('#hexOrientation').val(settings.hexOrientation);
+    $('#hexColums').val(settings.hexColums);
+    $('#hexRows').val(settings.hexRows);
+    $('#lineThickness').val(settings.lineThickness);
+    $('#hideCoords').prop('checked', settings.hideCoords);
+    $('#hideGrid').prop('checked', settings.hideGrid);
+}
+
+function initializeViewport(app, settings) {
+
+    let worldWidth = settings.hexColums * (settings.hexSize + (settings.hexSize / 2)) + (settings.hexSize / 2);
+    let worldHeight = settings.hexRows * (settings.hexSize * 1.731) + (settings.hexSize * 1.731 / 2);
+    if (settings.hexOrientation === 'pointy') {
+        worldWidth = settings.hexColums * (settings.hexSize * 1.731) + (settings.hexSize * 1.731 / 2);
+        worldHeight = settings.hexRows * (settings.hexSize + (settings.hexSize / 2)) + (settings.hexSize / 2);
+    }
+
+    const viewport = new Viewport.Viewport({
+        screenWidth: app.view.offsetWidth,
+        screenHeight: app.view.offsetHeight,
+        worldWidth: worldWidth,
+        worldHeight: worldHeight,
+
+        interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+    });
+
+    app.stage.addChild(viewport);
+
+    viewport
+        .drag()
+        .wheel()
+        .bounce();
+
+    return viewport;
+}
+
+function dec2hex (dec) {
+    return ('0' + dec.toString(16)).substr(-2)
+}
+
+// generateId :: Integer -> String
+function generateId (len) {
+    let arr = new Uint8Array((len || 40) / 2);
+    window.crypto.getRandomValues(arr);
+    return Array.from(arr, dec2hex).join('')
 }
